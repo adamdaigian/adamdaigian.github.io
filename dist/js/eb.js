2c7d244fb2005c98e8f805b16383a54455ea4f95
adamdaigian <adam.daigian@gmail.com>
2014-10-07 17:23:23 -0700

    finished first two sections, need to add advisor section

--- dist/js/eb.js
+++ dist/js/eb.js
@@ -5,14 +5,26 @@ $(document).ready(function() {
     mixpanel.track($(this).attr("track"))  
   });
 
-  $('.hex-nav').hover(function () {
-    $('.fa').addClass('blue');
-    $('.hexagon').addClass('white');
+  $(".resume").sparkle( {
+    color: "#FFFFFF",
+    count: 30,
+    overlap: 0,
+    speed: 1,
+    minSize: 4,
+    maxSize: 7,
+    direction: "both"
   });
 
-  $('.hex-fa').hover(function () {
-    console.log('hello world');
-    $('.fa').addClass('blue');
-    $('.hexagon').addClass('white');
-  });
+  // $('.hex-nav').hover(function () {
+  //   $('.fa').addClass('blue');
+  // }, function () {
+  //   console.log('hello world')
+  //   $('.fa').removeClass('blue');
+  // });
+
+  // $('.hex-nav .hex-fa').hover(function () {
+  //   console.log("hello world")
+  //   $('.fa').addClass('white');
+  //   $('.hexagon').addClass('.hexagon.white');
+  // });
 });
